077ba679ed1eaed8474309edd056eaa5219daafc
efraintinoco <oakcliff_21@yahoo.com>
2022-06-10 09:29:13 -0500

    chang date

--- src/main/java/com/etinoco/tasksapi/TasksapiApplication.java
+++ src/main/java/com/etinoco/tasksapi/TasksapiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TasksapiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TasksapiApplication.class, args);
+		SpringApplication.run(TasksapiApplication.class, args );
 	}
 
 }
